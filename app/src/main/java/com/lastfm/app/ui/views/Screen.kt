32e25f4fb729f4b6ead631c7a410e58092492377
kvgokul-git <kvgokul.git@gmail.com>
2022-03-29 16:42:23 +0100

    Re-structuring of code

--- app/src/main/java/com/lastfm/app/ui/views/Screen.kt
+++ app/src/main/java/com/lastfm/app/ui/views/Screen.kt
@@ -2,22 +2,31 @@ package com.lastfm.app.ui.views
 
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
-
-private const val KEY_ALBUM_NAME = "album"
-private const val KEY_ARTIST_NAME = "artist"
-private const val KEY_TRACK_NAME = "track"
+import com.lastfm.app.ui.views.Screen.Companion.KEY_ALBUM_NAME
+import com.lastfm.app.ui.views.Screen.Companion.KEY_ARTIST_NAME
+import com.lastfm.app.ui.views.Screen.Companion.KEY_TRACK_NAME
 
 sealed class Screen(val route: String) {
-    object Listing : Screen("listing")
-    object AlbumDetails : Screen("albumdetails")
-    object ArtistDetails : Screen("artistdetails")
-    object TrackDetails : Screen("trackdetails")
+    object Listing : Screen(KEY_LISTING)
+    object AlbumDetails : Screen(KEY_ALBUM_DETAILS)
+    object ArtistDetails : Screen(KEY_ARTIST_DETAILS)
+    object TrackDetails : Screen(KEY_TRACK_DETAILS)
 
     fun withArgs(vararg args: String?): String = buildString {
         append(route)
         args.forEach { arg ->
             append("/$arg")
         }
+    }
+
+    companion object {
+        const val KEY_ALBUM_NAME = "album"
+        const val KEY_ARTIST_NAME = "artist"
+        const val KEY_TRACK_NAME = "track"
+        private const val KEY_LISTING = "listing"
+        private const val KEY_ALBUM_DETAILS = "albumdetails"
+        private const val KEY_ARTIST_DETAILS = "artistdetails"
+        private const val KEY_TRACK_DETAILS = "trackdetails"
     }
 }
 
@@ -27,7 +36,7 @@ val albumDetailsScreenArguments = listOf(
         defaultValue = "love"
     },
     navArgument(KEY_ARTIST_NAME) {
-        type= NavType.StringType
+        type = NavType.StringType
         defaultValue = "love"
     }
 )
